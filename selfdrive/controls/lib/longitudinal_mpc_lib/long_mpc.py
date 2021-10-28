e5a8a0cb537ff0b0c780d1e57a58205d1bd40256
Harald Schafer <harald.the.engineer@gmail.com>
2021-10-28 10:04:27 -0700

    more x weights

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -215,7 +215,7 @@ class LongitudinalMpc():
       self.set_weights_for_lead_policy()
 
   def set_weights_for_lead_policy(self):
-    W = np.diag([0., .01, .0, 10., 1.])
+    W = np.diag([0., .03, .0, 10., 1.])
     Ws = np.tile(W[None], reps=(N,1,1))
     self.solver.cost_set_slice(0, N, 'W', Ws, api='old')
     # Setting the slice without the copy make the array not contiguous,
