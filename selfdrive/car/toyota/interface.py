4fb371a16229d66bbee3e9a237b8260faadc2fe6
Harald Schafer <harald.the.engineer@gmail.com>
2021-11-05 09:41:34 -0700

    mild toyota tune

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -324,20 +324,20 @@ class CarInterface(CarInterfaceBase):
       # Improved longitudinal tune
       ret.longitudinalTuning.deadzoneBP = [0., 8.05]
       ret.longitudinalTuning.deadzoneV = [.0, .14]
-      ret.longitudinalTuning.kpBP = [0., 5., 20.]
-      ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
-      ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-      ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
+      ret.longitudinalTuning.kpBP = [0., 35.]
+      ret.longitudinalTuning.kiBP = [0., 35.]
+      ret.longitudinalTuning.kpV = [1.0, 0.5]
+      ret.longitudinalTuning.kiV = [0.1, 0.05]
       ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
       ret.startingAccelRate = 6.0  # release brakes fast
     else:
       # Default longitudinal tune
       ret.longitudinalTuning.deadzoneBP = [0., 9.]
       ret.longitudinalTuning.deadzoneV = [0., .15]
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
+      ret.longitudinalTuning.kpBP = [0., 35.]
       ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
-      ret.longitudinalTuning.kiV = [0.54, 0.36]
+      ret.longitudinalTuning.kpV = [1.0, 0.5]
+      ret.longitudinalTuning.kiV = [0.1, 0.05]
 
     return ret
 
